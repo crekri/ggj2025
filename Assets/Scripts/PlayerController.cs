82aba533b1336aa85a22c8362aeb0cc859376cad
Aulork99 <aulork@outlook.com>
2025-01-25 18:40:36 +0800

    Player Move StateMachine

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using Bubble;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour, IPlayerController
+{
+    public PlayerStateMachine playerStateMachine;
+    public void SetMoveInput(Vector2 readValue)
+    {
+        playerStateMachine.CurrentState.SetMoveInput(readValue);
+    }
+
+    public void SetJumpInput(bool readValueAsButton)
+    {
+        playerStateMachine.CurrentState.SetJumpInput(readValueAsButton);
+    }
+
+    public bool IsFacingRight => playerStateMachine.CurrentState.IsFacingRight;
+    public void OnBubbleHit(BubbleHitInfo info)
+    {
+        playerStateMachine.OnBubbleHit(info);
+    }
+}
---
+++ Assets/Scripts/PlayerStateMachine.cs
@@ -0,0 +1,45 @@
+﻿using Bubble;
+using UnityEngine;
+
+public class PlayerStateMachine : MonoBehaviour
+{
+    [SerializeField]public PlayerTrapState playerTrapState;
+    public PlayerState CurrentState { get; private set; }
+
+    public void Awake()
+    {
+        foreach (var state in GetComponents<PlayerState>())
+        {
+            state.Init(this);
+        }
+		
+        TransitTo(GetComponent<PlayerFreeState>());
+    }
+
+    public void TransitTo(PlayerState playerState)
+    {
+        if (CurrentState != null)
+        {
+            CurrentState.OnExit();
+        }
+
+        CurrentState = playerState;
+        CurrentState.OnEnter();
+    }
+
+    public void FixedUpdate()
+    {
+        if (CurrentState != null)
+        {
+            CurrentState.MyFixedUpdate();
+        }
+    }
+	
+    public void OnBubbleHit(BubbleHitInfo info)
+    {
+        if (info.IsTrap)
+        {
+            TransitTo(playerTrapState);
+        }
+    }
+}
