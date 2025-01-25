fdd9ddf018f00e4c55428b56e40af3c58124d762
Aulork99 <aulork@outlook.com>
2025-01-25 14:17:35 +0800

    better jump and player params

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -4,21 +4,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class PlayerMovementController : MonoBehaviour
+public class PlayerMovementController : MonoBehaviour, IPlayerMovementController
 {
+	private PlayerParams playerParams;
 	private Rigidbody2D rb;
-
-	public float RunSpeed = 10f;
-	public float RunDamp = 0.15f;
-	public float JumpVelocity = 10f;
 	
+	public float RunDamp = 0.15f;
+
 	private Vector2 velocity;
 	private Vector2 moveInput;
+	private float jumpInput;
 
+	private bool isGrounded;
 
 	private void Awake()
 	{
 		rb = GetComponent<Rigidbody2D>();
+		playerParams = GetComponent<PlayerParams>();
+		
 	}
 
 	public void SetMoveInput(Vector2 input01)
@@ -26,15 +29,92 @@ public class PlayerMovementController : MonoBehaviour
 		moveInput = input01;
 	}
 
+	private bool _isJumpPressing;
+	private int _numOfJump; //reset by the raycast on ground 
 	public void SetJumpInput(bool isPressed)
 	{
-		if(isPressed)
-			rb.velocity = new Vector2(rb.velocity.x, JumpVelocity);
+		if (isPressed)
+		{
+			if (_numOfJump <= 0)
+			{
+				return;
+			}
+			_numOfJump--;
+			_isJumpPressing = true;
+		}
+		else
+		{
+			_isJumpPressing = false;
+			jumpHoldTime = 0;
+			jumpInput = 0;
+		}
+
+		if (isPressed)
+		{
+			Debug.Log("Jump");
+			rb.velocity = new Vector2(rb.velocity.x, playerParams.Stat.jumpVelocity);
+		}
 	}
 
+	public float jumpHoldTime;
+	public LayerMask groundLayer;
+
+	private void SetGround()
+	{
+		isGrounded = true;
+		_currentGravity = 0;
+		_numOfJump = playerParams.Stat.numberOfJump;
+	}
+
+	private void SetAirborne()
+	{
+		isGrounded = false;
+		_currentGravity = playerParams.Stat.gravity;
+		
+	}
+
+	public Transform groundCheck;
+	private float _currentGravity;
+	
+	
 	private void FixedUpdate()
 	{
-		var targetVelocity = new Vector2(moveInput.x * RunSpeed, rb.velocity.y);
-		rb.velocity = Vector2.SmoothDamp(rb.velocity, targetVelocity, ref velocity, RunDamp, Mathf.Infinity, Time.fixedDeltaTime);
+		if (_isJumpPressing)
+		{
+			jumpHoldTime += Time.deltaTime;
+			if (jumpHoldTime < playerParams.Stat.jumpHoldTime)
+			{
+				jumpInput += playerParams.Stat.jumpHoldStrengthRate * Time.fixedDeltaTime; //lerp required
+			}
+			else
+			{
+				jumpInput = 0;
+			}
+		}
+		
+		if (Physics2D.OverlapCircle(groundCheck.position, .1f, groundLayer))
+		{
+			if (!isGrounded)
+			{
+				SetGround();
+			}
+		}
+		else
+		{
+			SetAirborne();
+		}
+
+		if (!isGrounded)
+		{
+			_currentGravity += playerParams.Stat.airBorneGravityIncreaseRate * Time.fixedDeltaTime; //lerp function required
+		}
+		                       
+
+ 
+		var targetVelocity = new Vector2(moveInput.x * playerParams.Stat.moveVelocity, rb.velocity.y - _currentGravity + jumpInput * playerParams.Stat.jumpVelocity);
+		rb.velocity = new Vector2(Mathf.SmoothDamp(rb.velocity.x, targetVelocity.x, ref velocityX, RunDamp, Mathf.Infinity, Time.fixedDeltaTime), targetVelocity.y);
+
 	}
+
+	private float velocityX;
 }
---
+++ Assets/Scripts/PlayerParams.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct PlayerStat
+{   
+    //Movement
+    public float moveVelocity;
+    public float jumpVelocity;
+    //Jump
+    public int numberOfJump;
+    public float coyoteTime;
+    public float jumpBufferTime;
+    public float jumpHoldTime;
+    public float jumpHoldStrengthRate;
+    public float airBorneGravityIncreaseRate;
+    public float gravity;
+}
+//Store datas
+public class PlayerParams : MonoBehaviour
+{
+    public PlayerStat baseStat;
+
+    public PlayerStat Stat { get; private set; }
+
+    private HashSet<Modifier> _modifiers;
+
+    private void Awake()
+    {
+        Stat = baseStat;
+    }
+
+    private void RefreshStat()
+    {
+        Stat = baseStat;
+
+        foreach (var mod in _modifiers)
+        {
+            Stat = mod.ApplyTo(Stat);
+        }
+    }
+    public void AddModifer(Modifier mod)
+    {
+        _modifiers.Add(mod);
+        RefreshStat();
+    }
+
+    public void RemoveModifer(Modifier mod)
+    {
+        _modifiers.Remove(mod);
+        RefreshStat();
+    }
+
+}
+
+public abstract class Modifier
+{
+    public abstract PlayerStat ApplyTo(PlayerStat stat);
+    
+}
+
+public class ModifierSlow : Modifier
+{
+
+    public override PlayerStat ApplyTo(PlayerStat stat)
+    {
+        stat.moveVelocity -= stat.moveVelocity/2;
+           
+        return stat;
+    }
+}
+
+
