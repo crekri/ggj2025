1c8fb7ba6f49d4aae92be25922dae12bba4e19ce
Barshon Clinton Sarkar <barshon96@gmail.com>
2025-01-26 08:17:56 +0800

    audio manager update

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,82 +4,88 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    public static AudioManager AudioManagerInstance { get; private set; }
-
-    public AudioSource backgroundMusicSource;
-    public AudioSource sfxSourcePrefab;
-
-    private readonly List<AudioSource> sfxSources = new();
-
-    void Awake()
+    public List<AudioClip> audioClips;
+    public bool playOnStart = false;
+    public float volume = 1.0f;
+    public bool loop = false; 
+    public enum SFXType
     {
-        if (AudioManagerInstance == null)
-        {
-            AudioManagerInstance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
+        Random,
+        All
     }
+    public SFXType playMethod = SFXType.All;
 
     void Start()
     {
-        GameStateManager.GameStateChanged += OnGameStateChanged;
-    }
-
-    void OnDestroy()
-    {
-        GameStateManager.GameStateChanged -= OnGameStateChanged;
-    }
-
-    private void OnGameStateChanged(GameStateManager.GameState newState)
-    {
-        switch (newState)
+        if (playOnStart)
         {
-            case GameStateManager.GameState.Start:
-                break;
-            case GameStateManager.GameState.Play:
-                break;
-            case GameStateManager.GameState.Pause:
-                break;
-            case GameStateManager.GameState.End:
-                break;
+            PlaySFX();
         }
     }
 
-    public void PlayBackgroundMusic(AudioClip musicClip)
+    public void PlaySFX()
     {
-        if (backgroundMusicSource.clip != musicClip)
+        if (audioClips.Count == 0)
         {
-            backgroundMusicSource.clip = musicClip;
-            backgroundMusicSource.Play();
+            Debug.LogWarning("No audio clips provided");
+            return;
+        }
+        else if (audioClips.Count == 1)
+        {
+            LoadSFX(audioClips[0], volume, loop);
+        }
+        else
+        {
+            switch (playMethod)
+            {
+                case SFXType.Random:
+                    PlayRandomSFX(audioClips, volume, loop);
+                    break;
+                case SFXType.All:
+                    PlayAllSFX(audioClips, volume, loop);
+                    break;
+            }
+        }
+    }
+    public void LoadSFX(AudioClip sfxClip, float volume, bool loop)
+    {
+        AudioSource sfxSource = GetAvailableAudioSource();
+        sfxSource.clip = sfxClip;
+        sfxSource.loop = loop;
+        sfxSource.volume = volume;
+        sfxSource.Play();
+    }
+
+    public void PlayRandomSFX(List<AudioClip> sfxClips, float volume, bool loop)
+    {
+        if (sfxClips.Count == 0)
+        {
+            Debug.LogWarning("No audio clips provided");
+            return;
+        }
+
+        int randomIndex = Random.Range(0, sfxClips.Count);
+        AudioClip randomClip = sfxClips[randomIndex];
+        LoadSFX(randomClip, volume, loop);
+    }
+
+    public void PlayAllSFX(List<AudioClip> sfxClips, float volume, bool loop)
+    {
+        if (sfxClips.Count == 0)
+        {
+            Debug.LogWarning("No audio clips provided");
+            return;
+        }
+
+        for (int i = 0; i < sfxClips.Count; i++)
+        {
+            LoadSFX(sfxClips[i], volume, loop);
         }
     }
 
-    public void PlaySFX(AudioClip sfxClip)
+    private AudioSource GetAvailableAudioSource()
     {
-        AudioSource sfxSource = GetAvailableSFXSource();
-        sfxSource.PlayOneShot(sfxClip);
-    }
-
-    public void PlaySFX(AudioClip sfxClip, System.Action callback)
-    {
-        StartCoroutine(PlaySFXWithCallback(sfxClip, callback));
-    }
-
-    private IEnumerator PlaySFXWithCallback(AudioClip sfxClip, System.Action callback)
-    {
-        AudioSource sfxSource = GetAvailableSFXSource();
-        sfxSource.PlayOneShot(sfxClip);
-        yield return new WaitWhile(() => sfxSource.isPlaying);
-        callback?.Invoke();
-    }
-
-    private AudioSource GetAvailableSFXSource()
-    {
-        foreach (AudioSource source in sfxSources)
+        foreach (AudioSource source in GetComponents<AudioSource>())
         {
             if (!source.isPlaying)
             {
@@ -87,29 +93,26 @@ public class AudioManager : MonoBehaviour
             }
         }
 
-        AudioSource newSource = Instantiate(sfxSourcePrefab, transform);
-        sfxSources.Add(newSource);
-        return newSource;
+        return gameObject.AddComponent<AudioSource>();
     }
 
     public void StopAllSFX()
     {
-        foreach (AudioSource source in sfxSources)
+        foreach (AudioSource source in GetComponents<AudioSource>())
         {
             source.Stop();
         }
     }
 
-    public void StopBackgroundMusic()
+    public void StopSFX(int sourceIndex)
     {
-        backgroundMusicSource.Stop();
-    }
-
-    public void StopSFX(AudioSource sfxSource)
-    {
-        if (sfxSources.Contains(sfxSource))
+        AudioSource[] sources = GetComponents<AudioSource>();
+        if (sourceIndex < 0 || sourceIndex >= sources.Length)
         {
-            sfxSource.Stop();
+            Debug.LogWarning("Invalid audio source index");
+            return;
         }
+
+        sources[sourceIndex].Stop();
     }
 }
