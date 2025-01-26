162868b9726f66c45090e143436eb5c1fcc55d12
Aulork99 <aulork@outlook.com>
2025-01-26 10:39:43 +0800

    Update GamePlay Scene with new Level

---
+++ Assets/Scripts/TriggerElimation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class TriggerElimation : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        var colParent = col.GetComponentInParent<IPlayerController>();
+        if (colParent != null)
+        {
+            Destroy(col.transform.parent.gameObject);
+        }
+    }
+	
+}
